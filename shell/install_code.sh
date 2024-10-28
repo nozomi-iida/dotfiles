e11a4a068be4623abf4f9ff44420a9ca2b69f264
nozomi-iida <iida19990106@gmail.com>
2024-10-28 19:57:07 +0900

    fix: async vscode shell

--- shell/install_code.sh
+++ shell/install_code.sh
@@ -4,7 +4,8 @@ echo "Create vscode settings link."
 
 OS=$(uname)
 
-for codeFile in Code/*; do
+for codeFile in Code/User/*; do
+  echo "Linking $codeFile"
   if [ "$OS" = "Darwin" ]; then
     ln -snfv "$(pwd)/$codeFile" "$HOME/Library/Application Support/$codeFile"
   elif [ "$OS" = "Linux" ]; then
