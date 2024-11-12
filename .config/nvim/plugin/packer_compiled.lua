06db1f1ffb53886a80696f58bc06c5d9c1bb22f0
nozomi-iida <iida19990106@gmail.com>
2024-11-12 21:52:25 +0900

    feat: added packar_compiled to .gitignore

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -94,6 +94,11 @@ _G.packer_plugins = {
     path = "/home/nozomi/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
+  cmp_luasnip = {
+    loaded = true,
+    path = "/home/nozomi/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    url = "https://github.com/saadparwaiz1/cmp_luasnip"
+  },
   ["colorbuddy.nvim"] = {
     loaded = true,
     path = "/home/nozomi/.local/share/nvim/site/pack/packer/start/colorbuddy.nvim",
