fe1fcb096101c9bd9cadb8257d66bc41f8875c57
nozomi-iida <iida19990106@gmail.com>
2025-01-14 19:14:58 +0900

    Be able to make symbolic link with cursor

--- shell/install_code.sh
+++ shell/install_code.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -e
 
-echo "Create vscode settings link."
+echo "Create VSCode and Cursor settings link."
 
 OS=$(uname)
 
 for codeFile in Code/User/*; do
-  echo "Linking $codeFile"
+  filename=$(basename "$codeFile")
+  echo "Linking VSCode and Cursor: $filename"
   if [ "$OS" = "Darwin" ]; then
     ln -snfv "$(pwd)/$codeFile" "$HOME/Library/Application Support/$codeFile"
+    ln -snfv "$(pwd)/$codeFile" "$HOME/Library/Application Support/Cursor/User/$filename"
   elif [ "$OS" = "Linux" ]; then
     ln -snfv "$(pwd)/$codeFile" "$HOME/.config/$codeFile"
+    ln -snfv "$(pwd)/$codeFile" "$HOME/.config/Cursor/User/$filename"
   fi
 done
