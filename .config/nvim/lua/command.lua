8c6e61cdbf3ef2577c5a3a56f98d403ec595ed61
nozomi-iida <iida19990106@gmail.com>
2025-03-03 22:34:54 +0900

    feat: add command to write time

--- .config/nvim/lua/command.lua
+++ .config/nvim/lua/command.lua
@@ -13,3 +13,7 @@ function FileNameRelative()
 end
 
 vim.cmd('command! FileNameRelative lua FileNameRelative()')
+
+-- insetモードでCtrl-Dを2回押すと、YYYY-MM-DD HH:MM:SS形式の日付と時間を挿入
+vim.api.nvim_set_keymap('i', '<C-D><C-D>', '<C-R>=strftime("`%Y-%m-%d %H:%M:%S`")<CR><ESC>',
+  { noremap = true, silent = true })
