0bd732f20564a58ad3936ca8e44c47dd3bff347e
nozomi-iida <iida19990106@gmail.com>
2025-11-24 12:22:59 +0900

    feat: upgrade go version

--- shell/setup/programmings/go.sh
+++ shell/setup/programmings/go.sh
@@ -1,5 +1,5 @@
-wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz
-rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
-rm go1.23.4.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.25.2.linux-amd64.tar.gz
+rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.25.2.linux-amd64.tar.gz
+rm go1.25.2.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
