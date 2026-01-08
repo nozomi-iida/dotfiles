1eb4367e6f14a1ee591128a78e1856c481da6695
nozomi-iida <iida19990106@gmail.com>
2026-01-08 21:39:46 +0900

    feat: setup neotest

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,27 @@
+return {
+  -- Testing Framework
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      -- Vitest Adapter
+      "marilari88/neotest-vitest",
+    },
+    keys = {
+      { "<leader>tt", function() require("neotest").run.run() end,                     desc = "Run nearest test" },
+      { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end,   desc = "Run current file" },
+      { "<leader>ts", function() require("neotest").summary.toggle() end,              desc = "Toggle test summary" },
+      { "<leader>to", function() require("neotest").output.open({ enter = true }) end, desc = "Show test output" },
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-vitest"),
+        },
+      })
+    end,
+  },
+}
