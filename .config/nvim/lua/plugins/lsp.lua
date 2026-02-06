6e04e1ef952bf3a2148c5aaac9464396f4487388
nozomi-iida <iida19990106@gmail.com>
2026-02-06 18:03:56 +0900

    refactor: update lspsag config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -26,6 +26,33 @@ return {
   {
     'nvimdev/lspsaga.nvim',
     event = 'LspAttach',
+    config = function()
+      require('lspsaga').setup({
+        ui = {
+          winblend = 10,
+          border = 'rounded',
+          colors = {
+            normal_bg = '#002b36'
+          }
+        },
+        symbol_in_winbar = {
+          enable = false
+        }
+      })
+
+      local opts = { noremap = true, silent = true }
+      vim.keymap.set('n', '<C-j>', function()
+        require('lspsaga.diagnostic'):goto_next({
+          severity = { min = vim.diagnostic.severity.WARN }
+        })
+      end, opts)
+      vim.keymap.set('n', 'gl', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
+      vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
+      vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+      vim.keymap.set('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+      vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
+      vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
+    end,
   },
 
   -- Formatting & Linting
