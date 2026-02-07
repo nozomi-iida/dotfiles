a187c9f782f5ad9c65a1d8bbef8f4f4ed2aaa7df
nozomi-iida <iida19990106@gmail.com>
2026-02-07 23:32:16 +0900

    fix: conceal option

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -17,7 +17,7 @@ return {
     ft = 'markdown',
     init = function()
       vim.g.vim_markdown_folding_disabled = 1
-      vim.g.vim_markdown_conceal_code_blocks = 0
+      vim.g.vim_markdown_conceal = 0
     end,
     config = function()
       vim.cmd("map <Plug> <Plug>Markdown_Fold")
