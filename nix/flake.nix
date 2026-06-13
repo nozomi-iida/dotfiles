a8869ef2c670884755036d11a2e05d56cf089776
nozomi-iida <iida19990106@gmail.com>
2026-06-13 09:04:27 +0900

    feat: NixflakesとHome Managerの設定を追加

---
+++ nix/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Home Manager configuration of nozomi";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."nozomi" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [ ./home.nix ];
+      };
+    };
+}
---
+++ nix/home.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "nozomi";
+  home.homeDirectory = "/home/nozomi";
+
+  home.stateVersion = "26.05";
+
+  home.packages = [];
+
+  home.file = {};
+  home.sessionVariables = {
+  programs.home-manager.enable = true;
+}
